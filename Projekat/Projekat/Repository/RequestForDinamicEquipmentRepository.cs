89f34cdce1a7c650fd35033ec4c0878a54bdc5fe
miloscojder <miloscojder@gmail.com>
2021-05-05 01:11:14 +0200

    Odradjeni prihvatanje i odbijanje zahteva za odmor i CRUD za zahteve za dinamicku opremu

--- Projekat/Projekat/Repository/RequestForDinamicEquipmentRepository.cs
+++ Projekat/Projekat/Repository/RequestForDinamicEquipmentRepository.cs
@@ -4,41 +4,111 @@
  * Purpose: Definition of the Class Model.RequestForDinamicEquipment
  ***********************************************************************/
 
+
+using Model;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Repository
 {
    public class RequestForDinamicEquipmentRepository
    {
-      public Model.RequestForDinamicEquipment ReadRequest(int id)
+
+        private String FileLocation = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\Data\\RequestsForDynamicEquipment.json";
+        private List<RequestForDinamicEquipment> requestsForDinamicEquipment = new List<RequestForDinamicEquipment>();
+
+        public RequestForDinamicEquipmentRepository()
+        {
+
+            if (!File.Exists(FileLocation))
+            {
+                File.Create(FileLocation).Close();
+            }
+            using StreamReader r = new StreamReader(FileLocation);
+
+            string allData = r.ReadToEnd();
+            if (allData != "")
+            {
+                requestsForDinamicEquipment = JsonConvert.DeserializeObject<List<RequestForDinamicEquipment>>(allData);
+
+            }
+
+        }
+
+        public void WriteToJson()
+        {
+
+            string data = JsonConvert.SerializeObject(requestsForDinamicEquipment);
+            File.WriteAllText(FileLocation, data);
+
+        }
+
+        public int GenerateNextId()
+        {
+            try
+            {
+                int maxId = requestsForDinamicEquipment.Max(obj => obj.Id);
+                return maxId + 1;
+            }
+            catch
+            {
+                return 1;
+            }
+        }
+        public Model.RequestForDinamicEquipment ReadRequest(int id)
       {
-         // TODO: implement
-         return null;
-      }
+            RequestForDinamicEquipment requestForDinamicEquipment = new RequestForDinamicEquipment();
+            int index = requestsForDinamicEquipment.FindIndex(obj => obj.Id == id);
+            requestForDinamicEquipment = requestsForDinamicEquipment[index];
+            return requestForDinamicEquipment;
+        }
       
       public void Update(int id, String name)
       {
-         // TODO: implement
-      }
+            int index = requestsForDinamicEquipment.FindIndex(obj => obj.Id == id);
+            RequestForDinamicEquipment r = new RequestForDinamicEquipment();
+            requestsForDinamicEquipment[index].Name = name;
+            WriteToJson();
+        }
       
       public Boolean Delete(int id)
       {
-         // TODO: implement
-         return false;
-      }
+            int index = requestsForDinamicEquipment.FindIndex(obj => obj.Id == id);
+            if (index == -1)
+            {
+                return false;
+            }
+            requestsForDinamicEquipment.RemoveAt(index);
+            WriteToJson();
+            return true;
+        }
       
-      public void Save(int id)
+      public void Save(RequestForDinamicEquipment newRequestForDinamicEquipment)
       {
-         // TODO: implement
-      }
+            requestsForDinamicEquipment.Add(newRequestForDinamicEquipment);
+            WriteToJson();
+        }
       
-      public int AcceptingRequestForDinamycEquipment(int id, Model.StatusType newStatus, String explanation)
+      public Boolean AcceptingRequestForDinamycEquipment(int id, Model.StatusType newStatus)
       {
-         // TODO: implement
-         return 0;
+            int index = requestsForDinamicEquipment.FindIndex(obj => obj.Id == id);
+            requestsForDinamicEquipment[index].Status = newStatus;
+            WriteToJson();
+            if (requestsForDinamicEquipment[index].Status == StatusType.Accepted)
+            {
+                return true;
+            }
+
+            return false;
       }
-   
-      private String FileLocation;
-   
-   }
+
+      public List<RequestForDinamicEquipment> GetAll()
+      {
+          return requestsForDinamicEquipment;
+      }
+
+    }
 }
