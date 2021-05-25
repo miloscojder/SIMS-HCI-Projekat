524e96ccdbced47eaf3a2174705dd74905885322
AnamarijaD <anamarijadolovac@gmail.com>
2021-05-25 15:04:39 +0200

    Zakazivanje osnovnog renoviranja(Upravnik)

---
+++ Projekat/Projekat/Model/VerificationType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekat.Model
+{
+    public enum VerificationType
+    {
+        Waiting,
+        Accepted,
+        Rejected
+    }
+}
